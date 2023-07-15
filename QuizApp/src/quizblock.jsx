0943120528de241ab10e994f04b72c7aeb2c1927
Itoo <ryndak65@gmail.com>
2023-07-15 18:52:52 +0200

    Added fetch

--- QuizApp/src/quizblock.jsx
+++ QuizApp/src/quizblock.jsx
@@ -1,21 +1,33 @@
 import React from "react";
+import he from "he"
 
 export default function Quizblock() {
 
-    const [data, setData] = React.useState({});
+    const [data, setData] = React.useState([]);
+    const questions = [];
+    const fetchData = () => {
+        fetch('https://opentdb.com/api.php?amount=5')
+            .then(res => res.json())
+            .then(data => setData(data.results))
+            .catch(err => err)
+    }
 
     React.useEffect(() => {
-        fetch('https://opentdb.com/api.php?amount=5')
-        .then(res => res.json())
-            .then(data => setData(data))
-    }, [])
+        fetchData()
+    }, []);
 
-
-    console.log(data.results[0]);
+    console.log(data);
 
     return (
         <div>
-            <h1>Helloadass</h1>
+            {data.map((ele) => {
+                return (
+                    <div>
+                        <h2>{he.decode(ele.question)}</h2>
+                        <h3>{he.decode(ele.correct_answer)}</h3>
+                    </div>
+                )
+            })}
         </div>
     )
 }
