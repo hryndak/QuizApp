ffdfad7c9a96bfda86052e6e16d54c25e3c1ae3a
Itoo <ryndak65@gmail.com>
2023-07-18 17:29:41 +0200

    commit

---
+++ QuizApp/src/components/home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function StartPage() {
+
+    return (
+        <div className="home">
+            <div className="home-content">
+                <h1>Quizzical</h1>
+                <div className="inputs">
+                    <input type="text" />
+                    <input type="text" />
+                    <input type="text" />
+                </div>
+            </div>
+        </div>
+    )
+}
