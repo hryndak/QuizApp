a1a3cc91ba8a1902ea416cc6baf80a56797c7843
Itoo <ryndak65@gmail.com>
2023-07-25 19:19:08 +0200

    comit

--- QuizApp/src/components/quizblock.jsx
+++ QuizApp/src/components/quizblock.jsx
@@ -4,16 +4,23 @@ import './home.jsx'
 
 export default function Quizblock(props) {
 
-    console.log(props.data)
+    let arr = {};
+
+    
+    console.log(arr);
 
     return (
         <div>
             {props.data.map(ele => {
+
+                arr.incorrect_answers = ele.incorrect_answers;
                 return (
-                    <div>
+                    <div className="questions-and-answers">
                         <h1>{decode(ele.question)}</h1>
-                        <h2>{decode(ele.incorrect_answers)}</h2>
-                        <h3>{decode(ele.correct_answer)}</h3>
+                        <div className="answers">
+                            <h2>{decode(ele.incorrect_answers)}</h2>
+                            <h3 onClick={() => console.log("clicked right")} >{decode(ele.correct_answer)}</h3>
+                        </div>
                     </div>
                 )
             })}
